8367e90d323864dc4e74eae7c8b65826d1a2a7c6
Sabur7264 <49846108+Sabur7264@users.noreply.github.com>
2020-07-27 07:04:54 +0600

    Complete - Calculation Function Added,, Something Change.

--- asset/css-js/custom.js
+++ asset/css-js/custom.js
@@ -1,4 +1,22 @@
+// Audio Player
 function play() {
     var audio = document.getElementById("audio");
     audio.play();
+}
+
+// Input Value
+function number(value) {
+    document.calculatorForm.result.value += value;
+}
+
+// Clean Value
+function cle(value) {
+    document.calculatorForm.result.value = value;
+    document.calculatorForm.totalResult.value = value;
+}
+
+// Calculation
+function evalua() {
+//  var evalu = eval(document.calculatorForm.result.value);
+    document.calculatorForm.totalResult.value = eval(document.calculatorForm.result.value);
 }
